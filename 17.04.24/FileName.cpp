95474fe00dd5657b11bffab3611324a669ff200c
Your Name <you@example.com>
2024-05-29 14:09:45 +0300

    done

--- 17.04.24/FileName.cpp
+++ 17.04.24/FileName.cpp
@@ -12,11 +12,11 @@ int main() {
         return 0;
     }
 
-    /*ARRAY vector(file_txt);
+    ARRAY vector(file_txt);
     vector.print();
     vector.sorting();
     vector.print();
-    vector.to_binary("students_bin.txt");*/
+    vector.to_binary("students_bin.txt");
 
     std::ifstream file_bin("students_bin.txt", std::ios::binary);
     if (!file_bin.is_open()) {
@@ -33,40 +33,39 @@ int main() {
 
 void print_students_with_high_grades(std::ifstream& file_bin) {
     STUDENT student;
-    const int MAX_GROUPS = 100; 
-    const int MAX_STUDENTS_PER_GROUP = 100;
-    std::string groups[MAX_GROUPS][MAX_STUDENTS_PER_GROUP]; // Массив для хранения имен студентов по группам
-    int groupSize[MAX_GROUPS] = { 0 };
+    const int MAX_GROUPS = 100;
     bool found = false;
+    bool is_group_read = false;
+    int currentGroupIndex = -1;
 
-    // Считываем студентов из файла
     while (file_bin.read((char*)(&student), sizeof(STUDENT))) {
         double average = student.getter_average();
-        if (average >= 4.0) {
-            int groupIndex = student.getter_group() - 1; 
-            groups[groupIndex][groupSize[groupIndex]] = student.getter_FIO(); 
-            groupSize[groupIndex]++; 
-            found = true;
-        }
-    }
+        int groupIndex = student.getter_group() ;
 
-   
-    if (!found) {
-        std::cout << "No students with an average grade >= 4.0" << std::endl;
-    }
-    else {
-        for (int i = 0; i < MAX_GROUPS; ++i) {
-            if (groupSize[i] > 0) {
-                std::cout << "Group " << (i + 1) << ": ";
-                for (int j = 0; j < groupSize[i]; ++j) {
-                    std::cout << groups[i][j];
-                    if (j < groupSize[i] - 1) {
-                        std::cout << ", ";
-                    }
+        if (average >= 4.0) {
+             {
+                if (groupIndex != currentGroupIndex) {
+                    
+                    std::cout << std::endl;
+
+                    std::cout << "Group " << (groupIndex ) << ": ";
+                    currentGroupIndex = groupIndex;
+                    is_group_read = false;
                 }
-                std::cout << std::endl;
+                if (is_group_read) {
+                    std::cout << ", ";
+                }
+                std::cout << student.getter_FIO();
+                is_group_read = true;
+                found = true;
             }
         }
     }
-}
 
+    if (found && is_group_read) {
+        std::cout << std::endl;
+    }
+    else if (!found) {
+        std::cout << "No students with an average grade >= 4.0" << std::endl;
+    }
+}
